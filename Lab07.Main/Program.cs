bc555b351379181a329ed8bbcad3904f289f8ab8
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-10-15 19:53:08 -0600

    Main UI implemented

--- Lab07.Main/Program.cs
+++ Lab07.Main/Program.cs
@@ -7,8 +7,9 @@ public class Program
     static void Main()
     {
         DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        bool running = true;
 
-        while (true)
+        while (running)
         {
             Console.Clear();
             Console.Write(@"
@@ -16,7 +17,32 @@ public class Program
         a - add item to the front of the list
         b - add item to the back of the list
         p - print the current list
-        q - exit the program");
+        q - exit the program
+        ");
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.A:
+                    Console.Write("What would you like to add?: ");
+                    int frontValue = Convert.ToInt32(Console.ReadLine());
+                    list.AddFirst(frontValue);
+                    break;
+
+                case ConsoleKey.B:
+                    Console.Write("What would you like to add?: ");
+                    int backValue = Convert.ToInt32(Console.ReadLine());
+                    list.AddLast(backValue);
+                    break;
+
+                case ConsoleKey.P:
+                    foreach (var value in list)
+                        Console.WriteLine($"{value}");
+                    break;
+
+                case ConsoleKey.Q:
+                    running = false;
+                    break;
+            }
         }
     }
 }
