9d61806c6ea5c7d37e05cec7aa552dd174e6abc9
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-10-15 19:39:15 -0600

    Menu added

---
+++ Lab07.Main/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab07.Main;
+
+public class Program
+{
+    static void Main()
+    {
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+        while (true)
+        {
+            Console.Clear();
+            Console.Write(@"
+        LIST MENU
+        a - add item to the front of the list
+        b - add item to the back of the list
+        p - print the current list
+        q - exit the program");
+        }
+    }
+}
